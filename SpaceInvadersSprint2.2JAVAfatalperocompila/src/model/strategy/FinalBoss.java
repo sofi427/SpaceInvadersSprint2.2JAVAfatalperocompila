3dc994cd1ead012fad112fc3f44490591e37b2dc
maritoo0 <mlruanoverdeja@gmail.com>
2026-05-03 21:11:06 +0200

    Commit para poder hacer pull

---
+++ SpaceInvadersSprint2.2JAVAfatalperocompila/src/model/strategy/FinalBoss.java
@@ -0,0 +1,12 @@
+package model.strategy;
+
+public class FinalBoss extends NormalAlien {
+
+    public FinalBoss(int centerX, int centerY) {
+        super(centerX, centerY);
+    }
+
+    
+    
+    
+}
